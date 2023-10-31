bb8f1dec3efeecae14b3aba69668b62ff9dcddd4
etm-vy <etm-lav@spu.edu>
2023-10-31 11:17:56 -0700

    Some updates on mangas.js

--- routes/mangas.js
+++ routes/mangas.js
@@ -1,4 +1,5 @@
 const express = require('express');
+let manga = require('../models/manga');
 const router = express.Router();
 MangaSchema = require('../models/manga');
 
@@ -8,17 +9,30 @@ function HandleError(response, reason, message, code){
 }
 
 router.get('/', (request, response) => {
+    MangaSchema.find().then((mangas) => {
+        response.send(mangas);
+    }).catch((error) => {
+        HandleError(response, "error retrieving data", "get failed", 500);
+    });
+});
+
+router.post('/',(request, response) => {
     const mangaJSON = request.body;
-    if (!mangaJSON.name || !mangaJSON.chapter){
+    if (!mangaJSON.name || !mangaJSON.genre){
         HandleError(response, "missing information", "post data missing", 500);
-    }else {
+    } else{
         manga = new MangaSchema({
-            name: mangaJSON.name,
-            status: mangaJSON.status,
-            chapter: mangaJSON.chapter,
-            genre: mangaJSON.genre
-        }
-        );
-        manga.save()
+            name: MangaJSON.manga,
+            status: MangaJSON.status,
+            chapter: MangaJSON.chapter,
+            genre: MangaJSON.genre
+        });
+        manga.save().then((MangaSchema) => {
+            response.send({"id": manga.id});
+        }).catch((error) => {
+            response.send({"error": error});
+        });
     }
-})
+});
+
+module.exports = router;
