485d7f2c2ee793f09c6114fefed321f6ad9d6226
etm-vy <etm-lav@spu.edu>
2023-11-06 11:51:26 -0800

    update for reclaim cloud

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://127.0.0.1:27017/mangas';
+const url = 'mongodb://[admin:ANCcvl12114@]node13644-my-anime.us.reclaim.cloud/mangas';
 
 mongoose.connect(url, {useUnifiedTopology: true, useNewUrlParser: true});
 const db = mongoose.connection; 
